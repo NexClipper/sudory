776cb1d6f9a20b75434c5a4d530b25b855c75ae5
hyhecor <hyhecor@gmail.com>
2022-03-24 11:28:30 +0900

    clean up db column wth `api_version` - create table script - modify table script - insert data script

--- conf/script/server/create_predefined_client.sql
+++ conf/script/server/create_predefined_client.sql
@@ -1,3 +1,3 @@
 use sudory;
 
-INSERT INTO `client` (`created`, `updated`, `deleted`, `uuid`, `name`, `summary`, `api_version`, `cluster_uuid`) VALUES (NULL, NULL, NULL, '00000000000000000000000000000001', 'client-1', NULL, 'v1', '00000000000000000000000000000001');
+INSERT INTO `client` (`created`, `updated`, `deleted`, `uuid`, `name`, `summary`, `cluster_uuid`) VALUES (NULL, NULL, NULL, '00000000000000000000000000000001', 'client-1', NULL, '00000000000000000000000000000001');
--- conf/script/server/create_predefined_environment.sql
+++ conf/script/server/create_predefined_environment.sql
@@ -2,9 +2,9 @@ use sudory;
 
 DELETE FROM `environment`;
 
-INSERT INTO `environment` (`uuid`, `name`, `summary`, `api_version`, `value`, `created`, `updated`, `deleted`) VALUES ('cc6eeb942b9a4a9ca34dc4dfabc54275', 'cluster-token-signature-secret', '클러스터 토큰 시그니처 생성 시크릿 default=\'\'', 'v1', '', NOW(), NULL, NULL);
-INSERT INTO `environment` (`uuid`, `name`, `summary`, `api_version`, `value`, `created`, `updated`, `deleted`) VALUES ('77f7b2aeb0aa4254ad073ae7743291ab', 'client-token-signature-secret', '클라이언트 토큰 시그니처 생성 시크릿 default=\'\'', 'v1', '', NOW(), NULL, NULL);
-INSERT INTO `environment` (`uuid`, `name`, `summary`, `api_version`, `value`, `created`, `updated`, `deleted`) VALUES ('e2db6f6b08e94cb58bc6a35e244aaa29', 'bearer-token-signature-secret', 'bearer 토큰 시그니처 생성 시크릿 default=\'\'', 'v1', '', NOW(), NULL, NULL);
-INSERT INTO `environment` (`uuid`, `name`, `summary`, `api_version`, `value`, `created`, `updated`, `deleted`) VALUES ('af9a14a58b254d13ae69c065a27811b6', 'client-session-expiration-time', '클라이언트 세션 만료 시간 (초) default=\'60\'', 'v1', '60', NOW(), NULL, NULL);
-INSERT INTO `environment` (`uuid`, `name`, `summary`, `api_version`, `value`, `created`, `updated`, `deleted`) VALUES ('75531e760ee6423cb3a050ddcc83e275', 'client-config-poll-interval', '클라이언트 poll interval (초) default=\'15\'', 'v1', '15', NOW(), NULL, NULL);
-INSERT INTO `environment` (`uuid`, `name`, `summary`, `api_version`, `value`, `created`, `updated`, `deleted`) VALUES ('4e55651f63814b648f7284ab9113cf67', 'client-config-loglevel', '클라이언트 log level [\'debug\', \'info\', \'warn\', \'error\', \'fatal\'] default=\'debug\'', 'v1', 'debug', NOW(), NULL, NULL);
+INSERT INTO `environment` (`uuid`, `name`, `summary`, `value`, `created`, `updated`, `deleted`) VALUES ('cc6eeb942b9a4a9ca34dc4dfabc54275', 'cluster-token-signature-secret', '클러스터 토큰 시그니처 생성 시크릿 default=\'\'', '', NOW(), NULL, NULL);
+INSERT INTO `environment` (`uuid`, `name`, `summary`, `value`, `created`, `updated`, `deleted`) VALUES ('77f7b2aeb0aa4254ad073ae7743291ab', 'client-token-signature-secret', '클라이언트 토큰 시그니처 생성 시크릿 default=\'\'', '', NOW(), NULL, NULL);
+INSERT INTO `environment` (`uuid`, `name`, `summary`, `value`, `created`, `updated`, `deleted`) VALUES ('e2db6f6b08e94cb58bc6a35e244aaa29', 'bearer-token-signature-secret', 'bearer 토큰 시그니처 생성 시크릿 default=\'\'', '', NOW(), NULL, NULL);
+INSERT INTO `environment` (`uuid`, `name`, `summary`, `value`, `created`, `updated`, `deleted`) VALUES ('af9a14a58b254d13ae69c065a27811b6', 'client-session-expiration-time', '클라이언트 세션 만료 시간 (초) default=\'60\'', '60', NOW(), NULL, NULL);
+INSERT INTO `environment` (`uuid`, `name`, `summary`, `value`, `created`, `updated`, `deleted`) VALUES ('75531e760ee6423cb3a050ddcc83e275', 'client-config-poll-interval', '클라이언트 poll interval (초) default=\'15\'', '15', NOW(), NULL, NULL);
+INSERT INTO `environment` (`uuid`, `name`, `summary`, `value`, `created`, `updated`, `deleted`) VALUES ('4e55651f63814b648f7284ab9113cf67', 'client-config-loglevel', '클라이언트 log level [\'debug\', \'info\', \'warn\', \'error\', \'fatal\'] default=\'debug\'', 'debug', NOW(), NULL, NULL);
