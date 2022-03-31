7e04e3045c8436784ba8f0ba9feed56a9e35ddc6
JaehoonKim <jaehoon@nexclipper.io>
2022-03-31 20:26:13 +0900

    modify sql script

--- conf/script/server/sudory.sql
+++ conf/script/server/sudory.sql
@@ -1,5 +1,7 @@
 use `sudory`;
 
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `sudory` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+
 create user 'sudory'@'%' identified by 'sudory';
 create user 'sudory'@'localhost' identified by 'sudory';
 
