6caf0bb3e7484a6bcc491cb1bdc6b99ef29cec92
hyhecor <h@H3.localdomain>
2022-04-04 14:13:13 +0900

    P8s Rules; miss type

--- pkg/client/p8s/rules.go
+++ pkg/client/p8s/rules.go
@@ -3,6 +3,7 @@ package p8s
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 )
@@ -16,6 +17,24 @@ func (c *Client) Rules() (string, error) {
 	if err != nil {
 		return "", err
 	}
+	for _, group := range data.Groups {
+		for i, r := range group.Rules {
+			switch v := r.(type) {
+			case v1.RecordingRule:
+				group.Rules[i] = RecordingRule{
+					Type:          string(v1.RuleTypeRecording),
+					RecordingRule: v,
+				}
+			case v1.AlertingRule:
+				group.Rules[i] = AlertingRule{
+					Type:         string(v1.RuleTypeAlerting),
+					AlertingRule: v,
+				}
+			default:
+				fmt.Printf("unknown rule type %s", v)
+			}
+		}
+	}
 
 	b, err := json.Marshal(data)
 	if err != nil {
@@ -23,4 +42,16 @@ func (c *Client) Rules() (string, error) {
 	}
 
 	return string(b), nil
+}
+
+// AlertingRule models a alerting rule.
+type AlertingRule struct {
+	Type            string `json:"type"`
+	v1.AlertingRule `json:",inline"`
+}
+
+// RecordingRule models a recording rule.
+type RecordingRule struct {
+	Type             string `json:"type"`
+	v1.RecordingRule `json:",inline"`
 }
