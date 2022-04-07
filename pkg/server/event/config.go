5c540beb5a576be6a7882e44e147e3665646c58e
hyhecor <hyhecor@gmail.com>
2022-04-07 16:01:44 +0900

    change yaml key for event config

--- pkg/server/event/config.go
+++ pkg/server/event/config.go
@@ -50,17 +50,17 @@ type RabbitMQNotifierConfig struct {
 	Immediate  bool   `yaml:"immediate"`
 
 	//amqp.Publishing
-	MessageHeaders         map[string]interface{} `yaml:"message-headers,omitempty"`   // Application or header exchange table
-	MessageContentType     string                 `yaml:"message-content-type"`        // MIME content type
-	MessageContentEncoding string                 `yaml:"message-content-encoding"`    // MIME content encoding
-	MessageDeliveryMode    uint8                  `yaml:"message-delivery-mode"`       // queue implementation use - Transient (1) or Persistent (2)
-	MessagePriority        uint8                  `yaml:"message-Publishing-priority"` // queue implementation use - 0 to 9
-	MessageCorrelationId   string                 `yaml:"message-correlation-id"`      // application use - correlation identifier
-	MessageReplyTo         string                 `yaml:"message-eeply-to"`            // application use - address to to reply to (ex: RPC)
-	MessageExpiration      string                 `yaml:"message-expiration"`          // implementation use - message expiration spec
-	MessageMessageId       string                 `yaml:"message-message-id"`          // application use - message identifier
-	MessageTimestamp       bool                   `yaml:"message-timestamp"`           // application use - message timestamp
-	MessageType            string                 `yaml:"message-type"`                // application use - message type name
-	MessageUserId          string                 `yaml:"message-user-id"`             // application use - creating user id
-	MessageAppId           string                 `yaml:"message-app-id"`              // application use - creating application
+	MessageHeaders         map[string]interface{} `yaml:"message-headers,omitempty"` // Application or header exchange table
+	MessageContentType     string                 `yaml:"message-content-type"`      // MIME content type
+	MessageContentEncoding string                 `yaml:"message-content-encoding"`  // MIME content encoding
+	MessageDeliveryMode    uint8                  `yaml:"message-delivery-mode"`     // queue implementation use - Transient (1) or Persistent (2)
+	MessagePriority        uint8                  `yaml:"message-priority"`          // queue implementation use - 0 to 9
+	MessageCorrelationId   string                 `yaml:"message-correlation-id"`    // application use - correlation identifier
+	MessageReplyTo         string                 `yaml:"message-reply-to"`          // application use - address to to reply to (ex: RPC)
+	MessageExpiration      string                 `yaml:"message-expiration"`        // implementation use - message expiration spec
+	MessageMessageId       string                 `yaml:"message-message-id"`        // application use - message identifier
+	MessageTimestamp       bool                   `yaml:"message-timestamp"`         // application use - message timestamp
+	MessageType            string                 `yaml:"message-type"`              // application use - message type name
+	MessageUserId          string                 `yaml:"message-user-id"`           // application use - creating user id
+	MessageAppId           string                 `yaml:"message-app-id"`            // application use - creating application
 }
