60ab8c473881938f557d6da85844bd532f1f9161
Ian <krpuzx@nexclipper.io>
2022-04-28 15:23:23 +0900

    save session_uuid as '.sudory' file after handshake

--- pkg/client/fetcher/handshake.go
+++ pkg/client/fetcher/handshake.go
@@ -2,10 +2,14 @@ package fetcher
 
 import (
 	"encoding/json"
+	"os"
 	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 
 	"github.com/NexClipper/sudory/pkg/client/log"
 	authv1 "github.com/NexClipper/sudory/pkg/server/model/auth/v1"
+	sessionv1 "github.com/NexClipper/sudory/pkg/server/model/session/v1"
 	"github.com/NexClipper/sudory/pkg/version"
 )
 
@@ -26,6 +30,17 @@ func (f *Fetcher) HandShake() error {
 		return err
 	}
 	log.Debugf("Successed to handshake: received token(%s) for polling.", f.client.GetToken())
+
+	// save session_uuid from token
+	claims := new(sessionv1.ClientSessionPayload)
+	jwt_token, _, err := jwt.NewParser().ParseUnverified(f.client.GetToken(), claims)
+	if _, ok := jwt_token.Claims.(*sessionv1.ClientSessionPayload); !ok || err != nil {
+		log.Warnf("Failed to bind payload : %v\n", err)
+		return err
+	}
+	if err := writeFile(".sudory", []byte(claims.Uuid)); err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -52,4 +67,17 @@ func (f *Fetcher) RetryHandshake() {
 			return
 		}
 	}
+}
+
+func writeFile(filename string, data []byte) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := f.Write(data); err != nil {
+		return err
+	}
+	return nil
 }
