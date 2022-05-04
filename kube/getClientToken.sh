9daa7870b9b2fdd605c3272486098515498ba1c3
Sebastian Andrei Roman <seroman@deloittece.com>
2022-05-04 10:58:18 +0300

    [SR] Fix script to retrieve token for client

--- kube/getClientToken.sh
+++ kube/getClientToken.sh
@@ -4,23 +4,23 @@ SUDORY_SERVER_URL=$1
 CLUSTER_NAME="new-node-cluster"
 CLUSTER_DESCRIPTION='TEST-CLUSTER'
 
-echo "Retrieve user_uuid!"
-user_uuid=$(curl --silent -X POST $SUDORY_SERVER_URL/server/cluster \
+echo "Retrieve cluster_uuid!"
+cluster_uuid=$(curl --silent -X POST $SUDORY_SERVER_URL/server/cluster \
             -H "Content-Type: application/json"  \
             --data '{ "name": "'$CLUSTER_NAME'", "polling_option": { "additionalProp1": {} }, "summary": "'$CLUSTER_DESCRIPTION'" }' | jq -r '.uuid')
 
-if [[ -z user_uuid ]]
+if [[ -z cluster_uuid ]]
 then
-  echo "error on retrieving user_uuid"
+  echo "error on retrieving cluster_uuid"
 fi
 
 echo "Retrieve token!"
-token=$(curl --silent -X POST $SUDORY_SERVER_URL/server/token/cluster \
+token=$(curl --silent -X POST $SUDORY_SERVER_URL/server/cluster_token \
             -H "Content-Type: application/json"  \
-            --data '{ "name": "'$CLUSTER_NAME'", "user_uuid": "'$user_uuid'" }' | jq -r '.token')
+            --data '{ "name": "'$CLUSTER_NAME'", "cluster_uuid": "'$cluster_uuid'" }' | jq -r '.token')
 
 export S_SERVER_URL=$SUDORY_SERVER_URL
-export S_CLUSTER_ID=$user_uuid
+export S_CLUSTER_ID=$cluster_uuid
 export S_TOKEN=$token
 
 envsubst '${S_SERVER_URL} ${S_CLUSTER_ID} ${S_TOKEN}' < kube/k8s-deploy-nexclipper-sudory-client.yaml > kube/k8s-deploy-nexclipper-sudory-client.yaml_gen 
