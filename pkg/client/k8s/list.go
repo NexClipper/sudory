aae0ee3f6e833d81c1d89fd3c08bf2c41c20c423
Ian <krpuzx@nexclipper.io>
2022-06-09 13:10:44 +0900

    add k8s functinons to get/list resources - limitranges - podtemplates - replicationcontrollers - resourcequotas - serviceaccounts - controllerrevisions - ingressclasses - networkpolicies - csidrivers - csinodes - csistoragecapacities - volumeattachments - alertmanagers - podmonitors - probes - thanosrulers - alertmanagerconfigs - mutatingwebhookconfigurations - validatingwebhookconfigurations - customresourcedefinitions - apiservices - horizontalpodautoscalers - certificatesigningrequests - leases - endpointslices - runtimeclasses - poddisruptionbudgets - clusterrolebindings - clusterroles - rolebindings - roles - priorityclasses

--- pkg/client/k8s/list.go
+++ pkg/client/k8s/list.go
@@ -82,6 +82,31 @@ func (c *Client) ResourceList(gv schema.GroupVersion, resource string, params ma
 			if err != nil {
 				break
 			}
+		case "limitranges":
+			result, err = c.client.CoreV1().LimitRanges(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "podtemplates":
+			result, err = c.client.CoreV1().PodTemplates(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "replicationcontrollers":
+			result, err = c.client.CoreV1().ReplicationControllers(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "resourcequotas":
+			result, err = c.client.CoreV1().ResourceQuotas(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "serviceaccounts":
+			result, err = c.client.CoreV1().ServiceAccounts(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
 		default:
 			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
 		}
@@ -107,6 +132,11 @@ func (c *Client) ResourceList(gv schema.GroupVersion, resource string, params ma
 			if err != nil {
 				break
 			}
+		case "controllerrevisions":
+			result, err = c.client.AppsV1().ControllerRevisions(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
 		default:
 			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
 		}
@@ -117,6 +147,16 @@ func (c *Client) ResourceList(gv schema.GroupVersion, resource string, params ma
 			if err != nil {
 				break
 			}
+		case "ingressclasses":
+			result, err = c.client.NetworkingV1().IngressClasses().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "networkpolicies":
+			result, err = c.client.NetworkingV1().NetworkPolicies(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
 		default:
 			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
 		}
@@ -124,6 +164,26 @@ func (c *Client) ResourceList(gv schema.GroupVersion, resource string, params ma
 		switch resource {
 		case "storageclasses":
 			result, err = c.client.StorageV1().StorageClasses().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "csidrivers":
+			result, err = c.client.StorageV1().CSIDrivers().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "csinodes":
+			result, err = c.client.StorageV1().CSINodes().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "csistoragecapacities":
+			result, err = c.client.StorageV1().CSIStorageCapacities(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "volumeattachments":
+			result, err = c.client.StorageV1().VolumeAttachments().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
 			if err != nil {
 				break
 			}
@@ -147,6 +207,36 @@ func (c *Client) ResourceList(gv schema.GroupVersion, resource string, params ma
 			if err != nil {
 				break
 			}
+		case "alertmanagers":
+			result, err = c.mclient.MonitoringV1().Alertmanagers(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "podmonitors":
+			result, err = c.mclient.MonitoringV1().PodMonitors(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "probes":
+			result, err = c.mclient.MonitoringV1().Probes(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "thanosrulers":
+			result, err = c.mclient.MonitoringV1().ThanosRulers(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "monitoring.coreos.com/v1alpha1":
+		switch resource {
+		case "alertmanagerconfigs":
+			result, err = c.mclient.MonitoringV1alpha1().AlertmanagerConfigs(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
 		default:
 			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
 		}
@@ -159,6 +249,136 @@ func (c *Client) ResourceList(gv schema.GroupVersion, resource string, params ma
 			}
 		case "jobs":
 			result, err = c.client.BatchV1().Jobs(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "admissionregistration.k8s.io/v1":
+		switch resource {
+		case "mutatingwebhookconfigurations":
+			result, err = c.client.AdmissionregistrationV1().MutatingWebhookConfigurations().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "validatingwebhookconfigurations":
+			result, err = c.client.AdmissionregistrationV1().ValidatingWebhookConfigurations().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "apiextensions.k8s.io/v1":
+		switch resource {
+		case "customresourcedefinitions":
+			result, err = c.apiextv1client.ApiextensionsV1().CustomResourceDefinitions().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "apiregistration.k8s.io/v1":
+		switch resource {
+		case "apiservices":
+			result, err = c.aggrev1client.ApiregistrationV1().APIServices().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "autoscaling/v2":
+		switch resource {
+		case "horizontalpodautoscalers":
+			result, err = c.client.AutoscalingV2().HorizontalPodAutoscalers(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "certificates.k8s.io/v1":
+		switch resource {
+		case "certificatesigningrequests":
+			result, err = c.client.CertificatesV1().CertificateSigningRequests().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "coordination.k8s.io/v1":
+		switch resource {
+		case "leases":
+			result, err = c.client.CoordinationV1().Leases(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "discovery.k8s.io/v1":
+		switch resource {
+		case "endpointslices":
+			result, err = c.client.DiscoveryV1().EndpointSlices(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "node.k8s.io/v1":
+		switch resource {
+		case "runtimeclasses":
+			result, err = c.client.NodeV1().RuntimeClasses().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "policy/v1":
+		switch resource {
+		case "poddisruptionbudgets":
+			result, err = c.client.PolicyV1().PodDisruptionBudgets(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "rbac.authorization.k8s.io/v1":
+		switch resource {
+		case "clusterrolebindings":
+			result, err = c.client.RbacV1().ClusterRoleBindings().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "clusterroles":
+			result, err = c.client.RbacV1().ClusterRoles().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "rolebindings":
+			result, err = c.client.RbacV1().RoleBindings(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		case "roles":
+			result, err = c.client.RbacV1().Roles(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
+			if err != nil {
+				break
+			}
+		default:
+			err = fmt.Errorf("group version(%s)'s unsupported resource(%s)", gv.Identifier(), resource)
+		}
+	case "scheduling.k8s.io/v1":
+		switch resource {
+		case "priorityclasses":
+			result, err = c.client.SchedulingV1().PriorityClasses().List(context.TODO(), metav1.ListOptions{LabelSelector: labelsString})
 			if err != nil {
 				break
 			}
