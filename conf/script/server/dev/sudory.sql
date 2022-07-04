a93eb1678c41570dc8a6184154e15b60a74c333e
JaehoonKim <jaehoon@nexclipper.io>
2022-07-04 16:45:37 +0900

    modify containerize

--- conf/script/server/dev/sudory.sql
+++
@@ -1,6 +0,0 @@
-use `sudory`;
-create user 'sudory'@'%' identified by 'sudory';
-create user 'sudory'@'localhost' identified by 'sudory';
-
-GRANT ALL PRIVILEGES ON sudory.* TO 'sudory'@'%';
-GRANT ALL PRIVILEGES ON sudory.* TO 'sudory'@'localhost';
--- conf/script/server/entrypoint.sh
+++ conf/script/server/entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-initdb() {
-	/app/init-db.sh $SUDORY_DB_HOST $SUDORY_DB_PORT $SUDORY_DB_SCHEME /app/sudory.sql $SUDORY_DB_EXPORT_PATH $SUDORY_DB_ROOT_USERNAME $SUDORY_DB_ROOT_PASSWORD $SUDORY_DB_SERVER_USERNAME $SUDORY_DB_SERVER_PASSWORD
-}
-
 apprun() {
 	/app/sudory-server -config '/app/conf/sudory-server.yml'
 }
 
-initdb
 apprun
