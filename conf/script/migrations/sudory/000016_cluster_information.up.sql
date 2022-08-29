d1feceb818a533f7b7d33b35f02ab085f89b42c6
hyhecor <hyhecor@gmail.com>
2022-08-29 11:06:38 +0900

    v0.2.65
    - migrate:version=16

--- conf/script/migrations/sudory/000016_cluster_information.up.sql
+++ conf/script/migrations/sudory/000016_cluster_information.up.sql
@@ -4,7 +4,7 @@
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cluster_information` (
+CREATE TABLE IF NOT EXISTS `cluster_information` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `cluster_uuid` char(32) NOT NULL,
   `polling_count` int(10) DEFAULT NULL,
