294a7a5d4feb128d99d10cfc65a783c838e237d3
hyhecor <hyhecor@gmail.com>
2022-09-20 17:46:08 +0900

    service polling filter property
    add globvar(bc2cd0f95b6d4db68870d30862523a04)

--- pkg/server/status/globvar/default_values.go
+++ pkg/server/status/globvar/default_values.go
@@ -56,6 +56,14 @@ var defaultValueSet = map[Key]defaultValue{
 		clientConfigLoglevel = s
 		return nil
 	}},
+	KeyClientConfigServiceValidTimeLimit: {Uuid: "bc2cd0f95b6d4db68870d30862523a04", Value: "10", Summary: "Service Valid Time Limit (minute)", Setter: func(s string) error {
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return err
+		}
+		clientConfigServiceValidTimeLimit = i
+		return nil
+	}},
 	KeyEventNotifierStatusRotateLimit: {Uuid: "997c1631c9dd47f9a0c75448fb557ab0", Value: "20", Summary: "이벤트 알림 상태 rotate limit", Setter: func(s string) error {
 		i, err := strconv.Atoi(s)
 		if err != nil {
